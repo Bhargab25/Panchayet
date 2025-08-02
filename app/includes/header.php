3f22e039299ba64f6d7c5eef83e408af2768c496
kustav123 <47721564+kustav123@users.noreply.github.com>
2025-08-02 08:52:42 +0530

    fixCss

--- app/includes/header.php
+++ app/includes/header.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Freserganj Gram panchayat</title>
-    <!-- <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/fontawesome.min.css" /> -->
-    <link href="<?= BASE_URL ?>/assets/css/ux4g-v1.0.1.css" rel="stylesheet" />
-    <link href="<?= BASE_URL ?>/assets/css/html-template-1.css" rel="stylesheet" />
-    <link href="<?= BASE_URL ?>/assets/css/accesibility-style_v1.css" rel="stylesheet" />
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/main.css" />
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/js/slick/slick/slick.css" />
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/js/slick/slick/slick-theme.css" />
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/js/lightbox2/dist/css/lightbox.css" />
+    <!-- <link rel="stylesheet" href="/assets/css/fontawesome.min.css" /> -->
+    <link href="/assets/css/ux4g-v1.0.1.css" rel="stylesheet" />
+    <link href="/assets/css/html-template-1.css" rel="stylesheet" />
+    <link href="/assets/css/accesibility-style_v1.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/assets/css/main.css" />
+    <link rel="stylesheet" href="/assets/js/slick/slick/slick.css" />
+    <link rel="stylesheet" href="/assets/js/slick/slick/slick-theme.css" />
+    <link rel="stylesheet" href="/assets/js/lightbox2/dist/css/lightbox.css" />
   </head>
